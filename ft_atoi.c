177d872290f40a848b5c8c2085325f6fcf475a89
Rui Silva <97447789+rpedros02@users.noreply.github.com>
2024-01-07 21:56:06 +0000

    lib initiation.

---
+++ ft_atoi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rucorrei <rucorrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/07 20:48:43 by rucorrei          #+#    #+#             */
+/*   Updated: 2024/01/07 20:56:42 by rucorrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	nbr;
+	int	sign;
+
+	i = 0;
+	nbr = 0;
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
+		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (sign == -1 || str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+		nbr = (nbr * 10) + (str[i] - '0');
+	return (nbr * sign);
+}
---
+++ libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rucorrei <rucorrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/07 20:37:56 by rpcs02            #+#    #+#             */
+/*   Updated: 2024/01/07 21:07:00 by rucorrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int		ft_atoi(const char *str);
+size_t	ft_strlen(const char *str);
+char	ft_toupper(char c);
+char	ft_tolower(char c);
+
+#endif
